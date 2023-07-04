a302170ffde370f4643b7c74dcc284f0a412122d
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-07-04 16:31:28 +0100

    feature(): feature-database database of connection created separation of concern

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import http from "http";
+import { database } from "./connections/database";
+import dotenv from "dotenv";
+import log from "./logger/customlogger";
+import { app } from "./server";
+
+dotenv.config();
+
+const server = http.createServer(app);
+
+database().catch((err) => console.error(err));
+
+
+const PORT: string | undefined = process.env.PORT;
+
+
+server.listen(PORT , () => {
+    log.info(`Express is listening at http://localhost:${PORT}`);
+  });
+
+export default server
+
+
+
