3187ab463c4ab8cfa88a39cb5c1f31701b17d4b0
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-07-09 00:49:25 +0100

    feature(): feature for word search

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import { driver } from './drivers';
 import { vehicles } from './vehicles';
 import { auth } from './auth';
 import { user } from './users';
+import { search } from './query';
 
 export const routes = Router();
 
@@ -10,3 +11,4 @@ routes.use('/drivers', driver);
 routes.use('/vehicles', vehicles);
 routes.use('/users', auth);
 routes.use('/users', user);
+routes.use('/search', search)
