7a624ca3878f7e985429c8c390c0eb47ee37cb74
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-07-10 10:08:12 +0100

    Feature(): full Api documentation added

--- src/controllers/schedule.ts
+++ src/controllers/schedule.ts
@@ -95,6 +95,38 @@ export const createSchedule = async (
 	}
 };
 
-// export const getallSchedule = async (req: Request, res: Response, next: NextFunction) => {
-
-// }
+export const getallSchedule = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const allSchedule = await Schedule.find();
+		if (!allSchedule)
+			return next(
+				new AppError('You are able to perfrom this function', 404)
+			);
+		return res.status(202).json({
+			success: true,
+			message: 'All schedules has been retrieved',
+			data: allSchedule,
+		});
+	} catch (err: any) {
+		console.log(err);
+		return next(new AppError(`Service error ${err.message}`, 503));
+	}
+}
+export const getOneSchedule = async (req: Request, res: Response, next: NextFunction) => {
+			const {id} = req.params
+	try {
+		const oneSchedule = await Schedule.findOne({_id : id});
+		if (!oneSchedule)
+			return next(
+				new AppError('You are able to perfrom this function', 404)
+			);
+		return res.status(202).json({
+			success: true,
+			message: `This schedules with ${id} has been retrieved`,
+			data: oneSchedule,
+		});
+	} catch (err: any) {
+		console.log(err);
+		return next(new AppError(`Service error ${err.message}`, 503));
+	}
+}
--- src/routes/schedule.ts
+++ src/routes/schedule.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
-import { createSchedule } from '../controllers/schedule';
+import { createSchedule , getOneSchedule , getallSchedule} from '../controllers/schedule';
 
 export const schedule = Router();
 
 schedule.post('/scheduleDrivers===yts/:userId', createSchedule);
+schedule.get('/getAllSchedules/', getallSchedule)
+schedule.get('/getSchedule/:id', getOneSchedule)
